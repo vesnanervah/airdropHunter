5ce66de05bb6eaacb40ea655346639ce1b160050
vesnanervah <c.zwerew2012@gmail.com>
2023-10-31 13:27:22 +0300

    feat: add blockchains carousel controls listeners

--- src/app-main/carousel-block/carousel-block.ts
+++ src/app-main/carousel-block/carousel-block.ts
@@ -7,6 +7,9 @@ export default class CarouselBlock extends BaseView {
     private itemsListContainer: HTMLDivElement | undefined;
     private itemsList: HTMLDivElement | undefined;
     private GAP = 15;
+    private ITEM_WIDTH = 40;
+    private ANIMATION_TIME = 200;
+    private isAvailable = true;
 
     constructor() {
         super();
@@ -31,6 +34,9 @@ export default class CarouselBlock extends BaseView {
             center,
             rightControl
         );
+
+        leftControl.addEventListener('click', () => this.handlePrevClick());
+        rightControl.addEventListener('click', () => this.handleNextClick());
         return wrapper
     }
 
@@ -74,10 +80,58 @@ export default class CarouselBlock extends BaseView {
 
     //proper carousel work requires one more element inside, than user can view.
     private fillCarousel(index = 0) {
-        if (this.itemsListContainer.offsetWidth + this.carouselItems[0].offsetWidth >= this.itemsList.scrollWidth) {
+        if (this.itemsListContainer.offsetWidth + this.carouselItems[0].offsetWidth  >= this.itemsList.scrollWidth) {
             const clone = this.carouselItems[index].cloneNode(true);
+            this.carouselItems.push(clone as HTMLElement);
             this.itemsList.appendChild(clone);
             this.fillCarousel(this.carouselItems[index + 1] ? index + 1 : 0);
         }
+    }
+
+    private prependCarousel() {
+        this.carouselItems[this.carouselItems.length - 1].remove();
+        this.carouselItems.unshift(this.carouselItems[this.carouselItems.length - 1]);
+        this.itemsList.prepend(this.carouselItems[0]);
+        this.carouselItems.pop();
+    }
+
+    private appendCarousel() {
+        this.carouselItems[0].remove();
+        this.carouselItems.push(this.carouselItems[0]);
+        this.itemsList.append(this.carouselItems[0]);
+        this.carouselItems.shift();
+    }
+
+    private handlePrevClick() {
+        if (!this.isAvailable) {
+            return
+        }
+        const move = -1 * (this.ITEM_WIDTH + this.GAP);
+        this.prependCarousel();
+        this.itemsList.style.transform = `translateX${move}px`;
+        this.animateMove(move, 0, () => this.itemsList.style.transform = ``);
+    }
+
+    private handleNextClick() {
+        if (!this.isAvailable) {
+            return
+        }
+        const move = -1 * (this.ITEM_WIDTH + this.GAP);
+        this.animateMove(0, move, () => this.appendCarousel());
+
+    }
+
+    private animateMove(startPoint: number, endPoint: number, cb?: () => void) {
+        this.isAvailable = false;
+        const animation = this.itemsList.animate([
+            { transform: `translateX(${startPoint}px)`},
+            { transform: `translateX(${endPoint}px)`}
+        ], {
+            duration: this.ANIMATION_TIME
+        });
+        animation.onfinish = () => {
+            cb();
+            this.isAvailable = true;
+        };
     }
 }
